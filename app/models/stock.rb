316d83c6dd36b1ef3601965c7f09e4dc0805acd4
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2021-08-16 12:36:19 +0530

    tracking stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,7 +2,7 @@ class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
 
-  validates :name, :ticker, presence: true
+  validates :name, :ticker, :last_price, presence: true
 
 def self.new_lookup(ticker_symbol)
 client = IEX::Api::Client.new(
@@ -17,5 +17,8 @@ client = IEX::Api::Client.new(
   end
 end
 
+def self.check_db(ticker_symbol)
+  where(ticker: ticker_symbol).first
+end
 
 end
