7f996c789bab6fbb1abce23a0e4a5ba9a96fa765
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2021-08-16 10:02:24 +0530

    cleaning up nav

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,17 +1,21 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
 
- def self.new_lookup(ticker_symbol)
-  client = IEX::Api::Client.new(
-    publishable_token: Rails.application.credentials.iex[:publishable_key],
-    secret_token: Rails.application.credentials.iex[:secret_key],
-    endpoint: 'https://sandbox.iexapis.com/v1'
-    )
-    begin
-      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
-    rescue => exception
-      return nil
-    end
+  validates :name, :ticker, presence: true
+
+def self.new_lookup(ticker_symbol)
+client = IEX::Api::Client.new(
+  publishable_token: Rails.application.credentials.iex[:publishable_key],
+  secret_token: Rails.application.credentials.iex[:secret_key],
+  endpoint: 'https://sandbox.iexapis.com/v1'
+  )
+  begin
+    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+  rescue => exception
+    return nil
   end
+end
 
 
 end
