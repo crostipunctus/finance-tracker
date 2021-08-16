155a32e8697a75ce77d7b3c880ce3ec718b3c0e1
crostipunctus <63543350+crostipunctus@users.noreply.github.com>
2021-08-16 17:51:14 +0530

    untrack stock

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -11,5 +11,13 @@ class UserStocksController < ApplicationController
     redirect_to my_portfolio_path
   end
 
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user, stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice] = "You have removed #{stock.ticker} from your portfolio"
+    redirect_to my_portfolio_path
+  end
+
 
 end
--- app/models/user_stock.rb
+++ app/models/user_stock.rb
@@ -1,4 +1,8 @@
 class UserStock < ApplicationRecord
   belongs_to :user
   belongs_to :stock
+
+  
+
+
 end
